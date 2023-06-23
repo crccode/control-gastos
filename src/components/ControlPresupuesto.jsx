7f6e48d848a742ac86ff2d4770393b857549a91c
crccode <54750603+crccode@users.noreply.github.com>
2023-06-22 21:40:58 -0400

    FORMATEADO DEL PRESUPUESTO

---
+++ src/components/ControlPresupuesto.jsx
@@ -0,0 +1,33 @@
+import React from 'react'
+
+const ControlPresupuesto = ({presupuesto}) => {
+
+     // FORMATEAR EL PRESUPUESTO
+    const formatearCantidad = (cantidad) => {
+      return cantidad.toLocaleString('en-US', {
+        style: 'currency',
+        currency: 'USD'
+      })
+    }
+
+  return (
+    <div className="contenedor-presupuesto contenedor sombra dos-columnas">
+      <div >
+        <p>GRAFICO AQUI</p>
+      </div>
+      <div className="contenedor-presupuesto">
+        <p>
+            <span>Presupuesto: </span> {formatearCantidad(presupuesto)}
+        </p>
+        <p>     
+            <span>Disponible: </span> {formatearCantidad(0)}
+        </p>
+        <p>     
+            <span>Gastado: </span> {formatearCantidad(0)}
+        </p>
+      </div>
+    </div>
+  )
+}
+
+export default ControlPresupuesto;
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,12 +1,15 @@
 import React from 'react'
 import NuevoPresupuesto from './NuevoPresupuesto'
+import ControlPresupuesto from './ControlPresupuesto'
 
 const Header = ({presupuesto, setPresupuesto, isValidatePresupuesto,setIsValidatePresupuesto}) => {
   return (
     <header>
       <h1>Planificador de gastos</h1>
       { isValidatePresupuesto ? 
-        <p>VALIDO</p> :
+        <ControlPresupuesto
+          presupuesto = {presupuesto}
+        />:
         <NuevoPresupuesto
           presupuesto = {presupuesto}
           setPresupuesto = {setPresupuesto}
